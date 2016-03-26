841cf5568dae541c9fdcaee62801dd121a6578dd
Jaafar BENDRISS <bendriss-jaafar@live.fr>
2016-03-26 18:24:04 +0100

    Finishing the request script

--- env/testbed/monitoring/prometheus/pluginjaafar/get_metric.sh
+++ env/testbed/monitoring/prometheus/pluginjaafar/get_metric.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/bash 
+
+set -
+
+
+########################################################################
+# ----------------------- My Functions --------------------------------#    
+########################################################################
+
+
+###############################################
+#  start
+###############################################
 function start(){
 
 echo "Hello" 
@@ -11,62 +23,176 @@ echo "The mode 1 allow you to get basic metrics from the node targerted by prome
 read -p "Your choice ==> " OPTION 
 
 echo "
-Your choice was the activation of the mode $OPTION " 
+Your choice was the activation of the mode $OPTION and the argument is $1 ok" 
 }
 
-# send the query  can also use curl -o outputfile.txt
+# the range from time start to time end accordying to the RFC 3339
+#		SET the time range for the API request
+#
+function set_time() {
 
+LOCAL_ADDED_MINUTES=$2
+LOCAL_OPTION=$1
+
+case "$LOCAL_OPTION" in
+ "ADD")
+
+
+
+
+
+LOCAL_TT="T"
+LOCAL_ZZ="Z"
+
+# Using the date command
+LOCAL_RAW_TIME_DATE=$(date --rfc-3339=date --date=''$LOCAL_ADDED_MINUTES' minutes' --utc)
+LOCAL_RAW_TIME_NS=$(date --rfc-3339=ns --date=''$LOCAL_ADDED_MINUTES' minutes' --utc)
+# String Operations
+LOCAL_RAW_TIME_NS=$(echo "${LOCAL_RAW_TIME_NS:11:12}")
+LOCAL_RAW_TIME_DATE=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_TT)
+LOCAL_RAW_TIME_NS=$(echo $LOCAL_RAW_TIME_NS$LOCAL_ZZ)
+LOCAL_TIME_END=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_RAW_TIME_NS  )
+# Print the result
+TIME_END=$LOCAL_TIME_END
+
+ ;;
+  *)
+
+LOCAL_TT="T"
+LOCAL_ZZ="Z"
+# Using the date command
+LOCAL_RAW_TIME_DATE=$(date --rfc-3339=date --utc --universal)
+LOCAL_RAW_TIME_NS=$(date --rfc-3339=ns --utc --universal)
+# String Operations
+LOCAL_RAW_TIME_NS=$(echo "${LOCAL_RAW_TIME_NS:11:12}")
+LOCAL_RAW_TIME_DATE=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_TT)
+LOCAL_RAW_TIME_NS=$(echo $LOCAL_RAW_TIME_NS$LOCAL_ZZ)
+LOCAL_TIME_START=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_RAW_TIME_NS  )
+# Print the result
+TIME_START=$LOCAL_TIME_START
+
+     ;;
+esac
+}
+
+###############################################
+# option one
+###############################################
 function commands_api_option_one() {
+CMD_RET=$(curl -g 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T22:32:33.641Z&end=2016-03-25T22:52:33.641Z&step=15s')
+
+}
 
 
+###############################################
+# Option 2
+###############################################
+
+
+function commands_api_option_two() {
 # CMD_RET=$(curl -g 'http://localhost:9090/api/v1/series?match[]=up&match[]=go_memstats_next_gc_bytes{job="prometheus"}')
 # CMD_RET=$(curl -g 'http://localhost:9090/api/v1/label/job/values')
 # CMD_RET=$(curl 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T23:20:00.000Z&end=2016-03-25T23:32:00.000Z&step=15s')
 
-CMD_RET=$(curl -g 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T22:32:33.641Z&end=2016-03-25T22:52:33.641Z&step=15s')
+INDEX_FILE="1"
+OUTPUT_FILE="file${INDEX_FILE}"
+# date --rfc-3339=ns --utc --universal
 
-METRIC="process_virtual_memory_bytes process_cpu_seconds_total "
+LOCAL_INTERVAL_MINUTES=$1
+LOCAL_CMD_RET=$(curl -g 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T22:32:33.641Z&end=2016-03-25T22:52:33.641Z&step=15s -o ${OUTPUT_FILE} > error.log')
+
+LOCAL_METRIC="process_virtual_memory_bytes  process_cpu_seconds_total  go_memstats_next_gc_bytes  prometheus_local_storage_persistence_urgency_score
+go_gc_duration_seconds go_memstats_alloc_bytes go_memstats_mspan_inuse_bytes"
+
+INDEX="1"
+set_time  "STD" "0"
+set_time "ADD" $LOCAL_INTERVAL_MINUTES
+
+# 15 seconds
+STEP="15s"
 
 
+
+
+for M in $LOCAL_METRIC 
+do
+
+echo "the raw metris used are: ==>      $INDEX : $M 
+"
+
+OUTPUT_FILE="file${INDEX_FILE}"
+
+					while true
+					do
+					if [  ! -e $OUTPUT_FILE ]; then 
+					echo "create file $OUTPUT_FILE"
+					touch  $OUTPUT_FILE
+					echo " 
+					The output file of the API request $M is in ===> $OUTPUT_FILE  
+					"
+
+					#echo "debuggib : M= $M  time start = $TIME_START  step = $STEP  time end= $TIME_END "
+					# the range from time start to time end accordying to the RFC 3339
+					$(curl -g "http://localhost:9090/api/v1/query_range?query='$M'&start='$TIME_START'&end='$TIME_END'&step='$STEP' -o '$OUTPUT_FILE'")
+					echo " The API request sent is :  http://localhost:9090/api/v1/query_range?query=$M&start=$TIME_START&end=$TIME_END&step=$STEP -o $OUTPUT_FILE "
+					break
+
+					else
+					((INDEX_FILE++))
+					OUTPUT_FILE="file${INDEX_FILE}"
+					fi
+					done
+
+
+((INDEX++))
+done
 
 }
 
-# store the the response  in a JSON file format
 
+###############################################
+# store the the response  in a JSON file format
+###############################################
 function storage(){
 
 for VAR in  1 2 3 5 6 7 8 9 10
 do
 if [  ! -e file${VAR} ]; then 
-
-
 echo "create file $VAR"
-
-
 touch  file${VAR}
-
 echo "$CMD_RET" > file${VAR}  2>error.log
-
-
 exit 0
-
 else
-
-
 echo "file $VAR found"
-
 fi
 
 done
 }
 
 
-start
+
+
+
+
+
+
+
+
+
+
+
+########################################################################
+# ----------------------- The begining --------------------------------#    
+########################################################################			
+
+
+
+start $1
 
 case "$OPTION" in
  "1"*|"one")
 
- commands_api_option_one
+ commands_api_option_one $1
  storage
 
      ;;
@@ -75,7 +201,9 @@ case "$OPTION" in
 
 echo "choice 2"
 
-  ;;
+commands_api_option_two $1
+
+;;
 
  "3"*|"three")
 
--- env/testbed/monitoring/prometheus/pluginjaafar/test.sh
+++ env/testbed/monitoring/prometheus/pluginjaafar/test.sh
@@ -1,162 +1,71 @@
 #!/bin/bash
 
+# the range from time start to time end accordying to the RFC 3339
+#		SET the time range for the API request
+#
+function SET_TIME() {
 
-########################################################################
-# ----------------------- My Functions --------------------------------#    
-########################################################################
-
-
-###############################################
-#  start
-###############################################
-function start(){
-
-echo "Hello" 
-echo " This Program will allow you to send a group of query to the node you are monitoring and store the response 
-in a JSON file created dynamically as file1, file2, file3, etc. "
-echo " 
-This program has several mode "
-echo " options : 1 - 2 - 3"
-echo "The mode 1 allow you to get basic metrics from the node targerted by prometheus  "
-read -p "Your choice ==> " OPTION 
-
-echo "
-Your choice was the activation of the mode $OPTION " 
-}
-
-
-###############################################
-# send the query  can also use curl -o outputfile.txt
-###############################################
-function send_api_request(){
-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ COntinue here
-# local variable : the received raw metric
-LOCAL_METRIC=$1
-# the range from time start to time end accordying to the RF 3339
-LOCAL_TIME_START
-LOCAL_TIME_END
-# 15 seconds
-STEP="15"
-
-LOCAL_CMD_RET=$(curl -g 'http://localhost:9090/api/v1/query_range?query=${LOCAL_METRIC}&start=${LOCAL_TIME_START}Z&end=${LOCAL_TIME_END}Z&step=${STEP}s')
-
-
-}
-
-###############################################
-# option one
-###############################################
-function commands_api_option_one() {
-CMD_RET=$(curl -g 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T22:32:33.641Z&end=2016-03-25T22:52:33.641Z&step=15s')
-
-}
-
-
-###############################################
-# Option 2
-###############################################
-
-
-function commands_api_option_two() {
-# CMD_RET=$(curl -g 'http://localhost:9090/api/v1/series?match[]=up&match[]=go_memstats_next_gc_bytes{job="prometheus"}')
-# CMD_RET=$(curl -g 'http://localhost:9090/api/v1/label/job/values')
-# CMD_RET=$(curl 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T23:20:00.000Z&end=2016-03-25T23:32:00.000Z&step=15s')
-
-LOCAL_CMD_RET=$(curl -g 'http://localhost:9090/api/v1/query_range?query=up&start=2016-03-25T22:32:33.641Z&end=2016-03-25T22:52:33.641Z&step=15s')
-
-LOCAL_METRIC="process_virtual_memory_bytes  process_cpu_seconds_total  go_memstats_next_gc_bytes  prometheus_local_storage_persistence_urgency_score
-go_gc_duration_seconds go_memstats_alloc_bytes go_memstats_mspan_inuse_bytes"
-INDEX="1"
-for M in $LOCAL_METRIC 
-do
-echo "the raw metris used are: ==>      $INDEX : $M"
-send_api_request $M
-((INDEX++))
-done
-
-}
-
-
-###############################################
-# store the the response  in a JSON file format
-###############################################
-function storage(){
-
-for VAR in  1 2 3 5 6 7 8 9 10
-do
-if [  ! -e file${VAR} ]; then 
-
-
-echo "create file $VAR"
-
-
-touch  file${VAR}
-
-echo "$CMD_RET" > file${VAR}  2>error.log
-
-
-exit 0
-
-else
-
-
-echo "file $VAR found"
-
-fi
-
-done
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-########################################################################
-# ----------------------- The begining --------------------------------#    
-########################################################################			
-
-
-
-start
-
+ADDED_MINUTES=$2
+OPTION=$1
 case "$OPTION" in
- "1"*|"one")
+ "ADD")
 
- commands_api_option_one
- storage
 
-     ;;
 
- "2"*|"two")
 
-echo "choice 2"
 
-commands_api_option_two
+LOCAL_TT="T"
+LOCAL_ZZ="Z"
 
-;;
+# Using the date command
+LOCAL_RAW_TIME_DATE=$(date --rfc-3339=date --date=''$ADDED_MINUTES' minutes' --utc)
+LOCAL_RAW_TIME_NS=$(date --rfc-3339=ns --date=''$ADDED_MINUTES' minutes' --utc)
 
- "3"*|"three")
+# String Operations
+LOCAL_RAW_TIME_NS=$(echo "${LOCAL_RAW_TIME_NS:11:12}")
+LOCAL_RAW_TIME_DATE=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_TT)
+LOCAL_RAW_TIME_NS=$(echo $LOCAL_RAW_TIME_NS$LOCAL_ZZ)
+LOCAL_TIME_START=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_RAW_TIME_NS  )
 
-echo "choice 3"
-
+# Print the result
+echo "$LOCAL_TIME_START"
+ 
+ exit 0
  ;;
+ 
+ 
+ *)
 
-  *)
-echo "Option not supported yet"
+
+
+
+LOCAL_TT="T"
+LOCAL_ZZ="Z"
+
+# Using the date command
+LOCAL_RAW_TIME_DATE=$(date --rfc-3339=date --utc --universal)
+LOCAL_RAW_TIME_NS=$(date --rfc-3339=ns --utc --universal)
+
+# String Operations
+LOCAL_RAW_TIME_NS=$(echo "${LOCAL_RAW_TIME_NS:11:12}")
+LOCAL_RAW_TIME_DATE=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_TT)
+LOCAL_RAW_TIME_NS=$(echo $LOCAL_RAW_TIME_NS$LOCAL_ZZ)
+LOCAL_TIME_START=$(echo $LOCAL_RAW_TIME_DATE$LOCAL_RAW_TIME_NS  )
+
+# Print the result
+echo "$LOCAL_TIME_START"
+
 exit 0
      ;;
+
+ 
 esac
 
+}
 
-# After, I should use a table to gather all my query response
-exit 0
+PARA1=$1
+PARA2=$2
 
+ABC=SET_TIME $PARA1 $PARA2
 
+echo "My var is $ABC"
